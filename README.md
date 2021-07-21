d3f87a2e519a26380a4e59871a31c2ff972d674c
Christopher Laeri <claeri27@gmail.com>
2021-07-21 18:09:57 -0400

    🎉 cloned from next-basic-boilerplate

---
+++ README.md
@@ -0,0 +1,8 @@
+## Next Basic Boilerplate
+
+- React
+- Next
+- Eslint
+- Typescript
+- Prettier
+- Yarn 2 - PnP/No-Installation
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>**App Title**</title>
+        <meta name="description" content="**app description here**" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>stuff goes here</div>
+    </>
+  )
+}
